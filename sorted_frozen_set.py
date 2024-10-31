cbabfc0d5287b97a7c26f423561dc61413102673
Kinetics20 <kinetics20@gmail.com>
2024-10-31 22:59:59 +0100

    protocols_stests

--- sorted_frozen_set.py
+++ sorted_frozen_set.py
@@ -1,7 +1,10 @@
-class SortedFrozenSet:
+from collections.abc import Sequence
+
+
+class SortedFrozenSet(Sequence):
     def __init__(self, items=None):
-        self._items = (
-            set(items) if items is not None
+        self._items = tuple(sorted(
+            set(items)) if items is not None
             else set()
         )
 
@@ -16,5 +19,37 @@ class SortedFrozenSet:
 
     def __len__(self):
         return len(self._items)
+
+    def __iter__(self):
+        return iter(self._items)
+        # for item in self._items:
+        #     yield item
+
+    def __getitem__(self, index):
+        result = self._items[index]
+        return (
+            SortedFrozenSet(result)
+            if isinstance(index, slice)
+            else result
+        )
+        # print('\n')
+        # print(index)
+        # print(type(index))
+        # return self._items[index]
+
+    def __eq__(self, rhs):
+        if not isinstance(rhs, type(self)):
+            return NotImplemented
+        return self._items == rhs._items
+
+    # def __req__(self, lhs):
+    #     return not isinstance(lhs, type(self))
+
+    def __hash__(self):
+        return hash((type(self), self._items))
+
+
+    def __repr__(self):
+        return f'{type(self).__name__}({list(self._items) or ''})'
 
 
--- test_sorted_frozen_set.py
+++ test_sorted_frozen_set.py
@@ -1,3 +1,5 @@
+import pytest
+
 from sorted_frozen_set import SortedFrozenSet
 
 
@@ -74,6 +76,194 @@ def test_ten():
 def test_with_duplicates():
     s = SortedFrozenSet([5, 5, 5])
     assert len(s) == 1
+
+# iterable protocol
+# '__iter__' - return iterator object
+# or '__getitem__'
+
+@pytest.fixture
+def sfs():
+    return SortedFrozenSet([7, 2, 1, 1, 9])
+
+def test_iter(sfs):
+    iterator = iter(sfs)
+    assert next(iterator) == 1
+    assert next(iterator) == 2
+    assert next(iterator) == 7
+    assert next(iterator) == 9
+
+    with pytest.raises(StopIteration) as ctx:
+        next(iterator)
+
+def test_for_loop(sfs):
+    expected = [1, 2, 7, 9]
+
+    for index, item in enumerate(sfs):
+        assert item == expected[index]
+
+# Sequence protocol
+# - (container, sized, iterable)
+# '__getitem__'
+# retrieve an item by index (seq[index])
+# optionally - slicing (seq[start:stop:step])
+# locate an item by value (seq.index(item))
+# produce a reverse iterator (reversed(seq))
+# count items (seq.count(item))
+
+@pytest.fixture
+def sfs2():
+    return SortedFrozenSet([1, 4, 9, 13, 15])
+
+
+def test_index_zero(sfs2):
+    assert sfs2[0] == 1
+
+
+def test_index_last(sfs2):
+    assert sfs2[-1] == 15
+
+
+def test_index_three(sfs2):
+    assert sfs2[3] == 13
+
+
+def test_index_one_beyond_the_end(sfs2):
+    with pytest.raises(IndexError):
+        s = sfs2[5]
+
+
+
+def test_index_one_before_the_beginning(sfs2):
+    with pytest.raises(IndexError):
+        s = sfs2[-6]
+
+
+
+def test_slice_start(sfs2):
+    assert sfs2[:3] == SortedFrozenSet([1, 4, 9])
+
+
+def test_slice_to_end(sfs2):
+    assert sfs2[3:] == SortedFrozenSet([13, 15])
+
+
+def test_slice_empty(sfs2):
+    assert sfs2[10:] == SortedFrozenSet()
+
+
+def test_slice_arbitrary(sfs2):
+    assert sfs2[2:4] == SortedFrozenSet([9, 13])
+
+
+def test_slice_step(sfs2):
+    assert sfs2[0:5:2] == SortedFrozenSet([1, 9, 15])
+
+
+def test_slice_full(sfs2):
+    assert sfs2[:] == sfs2
+
+# __repr__
+
+def test_repr_empty():
+    s = SortedFrozenSet()
+    assert repr(s) == 'SortedFrozenSet()'
+
+
+def test_repr():
+    s = SortedFrozenSet([42, 41, 19])
+
+    assert repr(s) == "SortedFrozenSet([19, 41, 42])"
+
+# Equality
+
+def test_positive_equal():
+    assert SortedFrozenSet([4, 5, 6]) == SortedFrozenSet([6, 5, 4])
+
+
+def test_negative_equal():
+    assert not (SortedFrozenSet([4, 5, 6]) == SortedFrozenSet([1, 2, 3]))
+
+
+def test_type_mismatch():
+    assert not (SortedFrozenSet([1, 2, 3]) == [1, 2, 3])
+
+
+def test_identical():
+    s = SortedFrozenSet([1, 2, 3])
+    assert s == s
+
+# Inequality
+
+def test_positive_unequal():
+    assert SortedFrozenSet([4, 5, 6]) != SortedFrozenSet([1, 2, 3])
+
+
+def test_negative_unequal():
+    assert not (SortedFrozenSet([4, 5, 6]) != SortedFrozenSet([6, 5, 4]))
+
+
+def test_type_mismatch_unequal():
+    assert SortedFrozenSet([1, 2, 3]) != [1, 2, 3]
+
+
+def test_identical_unequal():
+    s = SortedFrozenSet([1, 2, 3])
+    assert not (s != s)
+
+
+# Hashable protocol
+# '__hash__', hash(obj)
+
+def test_equal_sets_have_the_same_hash_code():
+    assert hash(SortedFrozenSet([5, 2, 1, 4])) == hash(SortedFrozenSet([5, 2, 1, 4]))
+
+
+# Reversed
+# - '__reverse__'
+# - Fallback '__getitem__' + '__len__'
+
+def test_reversed():
+    s = SortedFrozenSet([1, 3, 5, 7])
+    r = reversed(s)
+
+    assert next(r) == 7
+    assert next(r) == 5
+    assert next(r) == 3
+    assert next(r) == 1
+
+    with pytest.raises(StopIteration):
+        next(r)
+
+# .index
+
+def test_index_positive():
+    s = SortedFrozenSet([1, 5, 8, 9])
+    assert s.index(8) == 2
+
+def test_index_negative():
+    s = SortedFrozenSet([1, 5, 8, 9])
+
+    with pytest.raises(ValueError):
+        s.index(15)
+
+# count
+
+def test_count_zero():
+    s = SortedFrozenSet([1, 5, 8, 9])
+    assert s.count(11) == 0
+
+def test_count_one():
+    s = SortedFrozenSet([1, 5, 8, 9])
+    assert s.count(8) == 1
+
+
+
+
+
+
+
+
+
 
 
 
